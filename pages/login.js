4a9e8e7b65b462ed9dfdf6125acf1b2bff0f60d0
HDhalukdogan <hd.halukdogan@hotmail.com>
2022-12-13 21:23:11 +0300

    magic

--- pages/login.js
+++ pages/login.js
@@ -5,10 +5,12 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 import styles from "../styles/Login.module.css";
+import { magic } from "../lib/magic-client";
 
 const Login = () => {
     const [userMsg, setUserMsg] = useState("");
     const [email, setEmail] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const router = useRouter();
 
@@ -17,14 +19,31 @@ const Login = () => {
         setEmail(e.target.value);
     }
 
-    const handleLoginWithEmail = () => {
+    const handleLoginWithEmail = async () => {
+        setIsLoading(true);
         if (email) {
-            if (email === "test") {
-                router.push("/");
+            if (email === "hd.halukdogan@hotmail.com") {
+                //  log in a user by their email
+                try {
+                    const didToken = await magic.auth.loginWithMagicLink({
+                        email,
+                    });
+                    console.log({ didToken });
+                    if (didToken) {
+                        setIsLoading(false);
+                        router.push('/');
+                    }
+                } catch (error) {
+                    // Handle errors if required!
+                    console.error("Something went wrong logging in", error);
+                    setIsLoading(false);
+                }
             } else {
+                setIsLoading(false);
                 setUserMsg("Something went wrong logging in")
             }
         } else {
+            setIsLoading(false);
             setUserMsg("Enter a valid email address");
         }
     }
@@ -60,7 +79,7 @@ const Login = () => {
                     />
                     <p className={styles.userMsg}>{userMsg}</p>
                     <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
-                        Sign In
+                        {isLoading ? "Loading..." : "Sign In"}
                     </button>
                 </div>
             </main>
